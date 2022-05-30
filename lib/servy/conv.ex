e1ce6afa96923dd583a3367269f7bf2c642b38d0
Alan Poveda <alsepoveda@gmail.com>
2022-05-30 16:42:07 -0300

    Make params to only POST method. The paramans you only take if make  a POST method, the other not metters

--- lib/servy/conv.ex
+++ lib/servy/conv.ex
@@ -1,20 +1,20 @@
 defmodule Servy.Conv do
-  defstruct path: "", status: nil, resp_body: "", method: "", params: %{}
+  defstruct path: "", status: nil, resp_body: "", method: "", params: %{}, headers: %{}
 
   def full_status(conv) do
-   "#{conv.status} #{status_reason(conv.status)}"
+    "#{conv.status} #{status_reason(conv.status)}"
   end
 
-    # Esta é uma forma bem diferente de achar o value da key num map. %{}[key]
-    defp status_reason(code) do
-      %{
-        200 => "OK",
-        201 => "Created",
-        204 => "Resource deleted successfully",
-        401 => "Unauthorized",
-        403 => "Forbidden",
-        404 => "Not Found",
-        500 => "Internal server error"
-      }[code]
-    end
+  # Esta é uma forma bem diferente de achar o value da key num map. %{}[key]
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      204 => "Resource deleted successfully",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal server error"
+    }[code]
+  end
 end
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -6,17 +6,37 @@ defmodule Servy.Parser do
     # Aqui esta separando em duas partes, o request e os parametros enviados
     [top,  params_string] = String.split(request, "\n\n")
     # Aqui serpara o Tipo  de requisição e os Headers info
-    [ request_line | _header_lines ] = String.split(top, "\n")
+    [ request_line | header_lines ] = String.split(top, "\n")
     # Aqui já são as infmações do request!
     [method, path, _ ] = String.split(request_line, " ")
 
-    params = parse_params(params_string)
+    IO.inspect header_lines
+
+    headers = parse_headers(header_lines, %{})
+
+    # Nesse caso ele somente vai pegar os parâmetros desse tipo, pois é do método POST
+    params = parse_params(headers["Content-Type"], params_string)
 
     # Isto é o retorno dessa função, um map com a arr
-    %Conv{method: method, path: path, params: params}
+    %Conv{method: method, path: path, params: params, headers: headers}
   end
 
-  defp parse_params(params_string) do
+  # Pegar os dados do header e transformar em map, usando recursividade
+  defp parse_headers([head | tail], headers) do
+    # Aqui faz a separação do Key e value
+    [key, value] = String.split(head, ": ")
+    # Criação do map, e ele recebe o map anterior, dessa forma ele adiciona!
+    header = Map.put(headers, key, value)
+    # Recusividade!, chama denovo somente a tail e começa o processo novamente
+    parse_headers(tail, header)
+  end
+
+  defp parse_headers([], headers), do: headers
+
+  defp parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim() |> URI.decode_query()
   end
+
+  #Essa daqui ignora tudo o resto que seja de outro método sem ser o POST e retorna um map vazio
+  defp parse_params(_,_), do: %{}
 end
