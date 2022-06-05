5556e9ac43327a36dfe62be6b478753ba81925fd
Alan Poveda <alsepoveda@gmail.com>
2022-06-05 12:39:43 -0300

    Crate a database bears

--- lib/servy/wildthings.ex
+++ lib/servy/wildthings.ex
@@ -1,16 +1,33 @@
 defmodule Servy.Wildthings do
   alias Servy.Bear
 
+  @bd_json Path.expand("../../db", __DIR__)
+
+  # def list_bears do
+  #   [
+  #     %Bear{id: 1, name: "Pequena", type: "Japanese", hibernating: true},
+  #     %Bear{id: 2, name: "David", type: "Colombian", hibernating: true},
+  #     %Bear{id: 3, name: "Luz", type: "Colombian", hibernating: true},
+  #     %Bear{id: 4, name: "Gaby", type: "Colombian", hibernating: true},
+  #     %Bear{id: 5, name: "Alan", type: "Brazilian", hibernating: true},
+  #     %Bear{id: 6, name: "Adma", type: "Brazilian", hibernating: true},
+  #     %Bear{id: 7, name: "Neomax", type: "Colombian", hibernating: true},
+  #   ]
+  # end
+
   def list_bears do
-    [
-      %Bear{id: 1, name: "Pequena", type: "Japanese", hibernating: true},
-      %Bear{id: 2, name: "David", type: "Colombian", hibernating: true},
-      %Bear{id: 3, name: "Luz", type: "Colombian", hibernating: true},
-      %Bear{id: 4, name: "Gaby", type: "Colombian", hibernating: true},
-      %Bear{id: 5, name: "Alan", type: "Brazilian", hibernating: true},
-      %Bear{id: 6, name: "Adma", type: "Brazilian", hibernating: true},
-      %Bear{id: 7, name: "Neomax", type: "Colombian", hibernating: true},
-    ]
+    @bd_json
+    |> Path.join("bears.json")
+    |> read_json()
+    |> Poison.decode!(as: %{"bears" => [%Bear{}]})
+    |> Map.get("bears")
+  end
+
+  defp read_json(source) do
+    case File.read(source) do
+      {:ok, contents} -> contents
+      {:error, reason} -> IO.inspect("Error to read file #{source}, reason #{reason}")
+    end
   end
 
   def get_bear(id) when is_integer(id) do
@@ -20,6 +37,4 @@ defmodule Servy.Wildthings do
   def get_bear(id) when is_binary(id) do
     id |> String.to_integer() |> get_bear()
   end
-
-
 end
