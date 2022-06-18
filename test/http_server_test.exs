b7d68a4f21ae5855aab10ebc6d2df637a3d3cd3f
Alan Poveda <alsepoveda@gmail.com>
2022-06-18 04:45:58 -0300

    test a multiple request

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -72,12 +72,36 @@ defmodule HttpServerTest do
 
     1..5
     |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end) end)
-    |> Enum.map(&Task.await/1)
-    |> Enum.map(&assert_successful_response/1)
+    |> Enum.map(&Task.await(&1))
+    |> Enum.map(&assert_successful_response(&1))
   end
 
   defp assert_successful_response({:ok, response}) do
     assert response.status_code == 200
     assert response.body == "🐻🐻🐻🐻🐻Bears, Lions, Tigers🐻🐻🐻🐻🐻"
   end
+
+
+  test "accepts multiple request using async functions" do
+    spawn(HttpServer, :start, [4000])
+
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/bears",
+      "http://localhost:4000/bears/1",
+      "http://localhost:4000/wildlife",
+      "http://localhost:4000/api/bears"
+    ]
+
+    urls
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
+    |> Enum.map(&Task.await(&1))
+    |> Enum.map(&assert_successful_multiple(&1))
+
+  end
+
+  defp assert_successful_multiple({:ok, response}) do
+    assert response.status_code == 200
+  end
+
 end
